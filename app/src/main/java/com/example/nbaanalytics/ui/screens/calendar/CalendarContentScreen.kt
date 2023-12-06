13c3c10ca06d0fa9d4be4a1e4da76a4a3d5ab57c
Vsevolod Iskra <sk4ilegend@gmail.com>
2023-12-06 17:11:44 +0200

    Added fetch todayGames logic

--- app/src/main/java/com/example/nbaanalytics/ui/screens/calendar/CalendarContentScreen.kt
+++ app/src/main/java/com/example/nbaanalytics/ui/screens/calendar/CalendarContentScreen.kt
@@ -1,14 +1,22 @@
 package com.example.nbaanalytics.ui.screens.calendar
 
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.hilt.navigation.compose.hiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.time.LocalDate
+
+private var todayDate = LocalDate.now().toString()
 
 @Composable
 fun CalendarContentScreen(viewModel: CalendarContentViewModel = hiltViewModel()) {
+    val scope = rememberCoroutineScope()
     viewModel.viewState.buildUI(
         onRefresh = {
-
+            scope.launch(Dispatchers.IO) {
+                viewModel.fetchData(todayDate)
+            }
         },
         ui = {
             ContentScreen()
@@ -17,7 +25,7 @@ fun CalendarContentScreen(viewModel: CalendarContentViewModel = hiltViewModel())
 
 @Composable
 private fun ContentScreen() {
-    TopBarCalendar {
-
+    TopBarCalendar {chosenDate ->
+        todayDate = chosenDate.toString()
     }
 }
--- app/src/main/java/com/example/nbaanalytics/ui/screens/calendar/CalendarContentViewModel.kt
+++ app/src/main/java/com/example/nbaanalytics/ui/screens/calendar/CalendarContentViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.nbaanalytics.ui.screens.calendar
 
-import android.graphics.pdf.PdfDocument.Page
 import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -8,17 +7,44 @@ import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.nbaanalytics.data.repository.TodayGamesRepository
-import com.example.nbaanalytics.network.services.GamesNetworkService
+import com.example.nbaanalytics.network.base.Error
 import com.example.nbaanalytics.ui.state.PageViewState
+import com.example.network_services.network.models.GamesResponse
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import java.time.LocalDate
 import javax.inject.Inject
 
 @HiltViewModel
-class CalendarContentViewModel @Inject constructor(private val todayGamesRepository: TodayGamesRepository) : ViewModel() {
+class CalendarContentViewModel @Inject constructor(private val todayGamesRepository: TodayGamesRepository) :
+    ViewModel() {
 
-    var viewState by mutableStateOf<PageViewState>(PageViewState.LoadedContent)
+    var viewState by mutableStateOf<PageViewState>(PageViewState.LoadingContent)
         private set
 
+    var todayGamesData by mutableStateOf(listOf<GamesResponse>())
+        private set
+
+    suspend fun fetchData(todayDate: String) {
+        todayGamesRepository.getTodayGames(todayDate).handleResult(::handleSuccess, ::handleFailure)
+    }
+
+    private val todayDate = LocalDate.now().toString()
+
+    init {
+        viewModelScope.launch {
+            fetchData(todayDate)
+        }
+    }
+
+    private fun handleSuccess(data: Any) {
+        val games = (data as List<GamesResponse>)
+        todayGamesData = games
+        viewState = PageViewState.LoadedContent
+    }
+
+    private fun handleFailure(error: Error) {
+        Log.d("fesf", "handleError: ")
+        viewState = PageViewState.Error(error.toString())
+    }
 }
